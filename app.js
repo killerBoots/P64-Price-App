3088fce29a0cd757852a8715d5c9f451ac10c4d2
Nathan Dickow <ndickow@gmail.com>
2018-06-07 13:57:43 -0400

    Working prototype.

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 const axios = require('axios');
+const express = require('express');
+const app = express();
+let avgRnd = '';
+const priceArray = [];
 
 axios.get('https://www.gunbroker.com/api/items?categories=978&Sort=9&PageIndex=1&PageSize=96&keywords=polish+p64')
   .then(function (response) {
-    maximumPrice = response.data.maxPrice;
-    minimumPrice = response.data.minPrice;
-    volume = response.data.count;
-    buyNow = {};
-    buyNow = response.data.results.buyNowPrice;
-    console.log('Max Price: ' + maximumPrice, 'Minimum Price: ' + minimumPrice, 'Daily Volume: ' + volume);
-    console.log(buyNow);
-  })
+    rawData = response.data.results;
+    //const priceArray = [];
+    for (var i = 0; i < rawData.length; i++) {
+      priceArray.push(rawData[i].bidPrice);
+    }
+
+    const index = priceArray.indexOf(0);
+    if (index > -1) {
+      priceArray.splice(index);
+    }
+
+    const sum = priceArray.reduce(function (acc, cur) {
+      return acc + cur;
+    }, 0);
+    const avg = sum / priceArray.length;
+    avgRnd = Math.round(avg);
+    console.log(avgRnd);
+
+    })
   .catch(function (error) {
     console.log(error);
-  });
+  })
+
+console.log(avgRnd);
+
+app.get('/', function (req, res) {
+  res.sendStatus(avgRnd);
+})
+
+app.listen(3000, () => console.log('Listening on localhost:3000'))
